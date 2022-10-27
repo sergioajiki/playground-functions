2ae3a1cdb6a07d880377cdf436280c6212852402
Sergio <sergioajk@hotmail.com>
2022-10-27 20:24:42 -0300

    atualização

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,5 @@
 // Desafio 1
 function compareTrue(para1, para2) {
-
   if (para1 && para2) {
     return true;
   } else {
@@ -15,24 +14,19 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(Frase) {
-
   return Frase.split(' ');
-
 }
 // console.log(splitSentence('vamo que vamo'));
 
 // Desafio 4
 function concatName(array) {
-
   return `${array[array.length - 1]}, ${array[0]}`;
-
 }
 // console.log(concatName(['captain', 'my', 'captain']))
 
 
 // Desafio 5
 function footballPoints(wins, ties) {
-
   return wins * 3 + ties * 1;
 }
 // console.log(footballPoints(0, 0));
@@ -45,21 +39,20 @@ function highestCount(number) {
       numMaior = number[index];
     }
   }
-  let countNumMaior = 0
+  let countNumMaior = 0;
   for (let index2 = 0; index2 < number.length; index2 += 1) {
     if (number[index2] === numMaior) {
-      countNumMaior += 1
+      countNumMaior += 1;
     }
   }
-  return countNumMaior
+  return countNumMaior;
 }
 // console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-
   if (Math.abs(mouse - cat1) === Math.abs(mouse - cat2)) {
-    return `os gatos trombam e o rato foge`
+    return `os gatos trombam e o rato foge`;
   } else if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
     return `cat1`;
   } else {
@@ -79,7 +72,6 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(numbers) {
   let resultado = [];
   for (let index = 0; index < numbers.length; index += 1) {
-
     if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
       resultado[index] = `fizzBuzz`;
     } else if (numbers[index] % 3 === 0 && numbers[index] % 5 !== 0) {
@@ -90,10 +82,12 @@ function fizzBuzz(numbers) {
       resultado[index] = `bug!`;
     }
 }
-  return resultado
+  return resultado;
 }
-
 // console.log(fizzBuzz([2, 15, 7, 9, 45]));
+
+
+
 
 // Desafio 9
 function encode() {
