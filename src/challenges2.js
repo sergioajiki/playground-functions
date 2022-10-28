bd88c201d53546b7d9ce5bce5630731376cd48b1
Sergio <sergioajk@hotmail.com>
2022-10-28 18:48:48 -0300

    requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,17 +5,14 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  let validaTriangulo = 0
-  if (lineA < lineB && lineA < lineC) {
-    validaTriangulo = (lineA < (lineB + lineC)) && (lineA > Math.abs(lineB - lineC)) || ((lineA < (lineB + lineC)) && (lineB < (lineA + lineC)) && (lineC < (lineA + lineB)));
-  } else if (lineB < lineA && lineB < lineC) {
-    validaTriangulo = (lineB < (lineA + lineC)) && (lineB > Math.abs(lineA - lineC)) || ((lineA < (lineB + lineC)) && (lineB < (lineA + lineC)) && (lineC < (lineA + lineB)));
-  } else if (lineC < lineA && lineC < lineB) {
-    validaTriangulo = (lineC < (lineA + lineB)) && (lineC > Math.abs(lineA - lineB)) || ((lineA < (lineB + lineC)) && (lineB < (lineA + lineC)) && (lineC < (lineA + lineB)));
+  if (lineA <= lineB && lineA <= lineC) {
+    return validaTriangulo = (lineA < (lineB + lineC)) && (lineA > Math.abs(lineB - lineC)) || ((lineA < (lineB + lineC)) && (lineB < (lineA + lineC)) && (lineC < (lineA + lineB)));
+  } else if (lineB <= lineA && lineB <= lineC) {
+    return validaTriangulo = (lineB < (lineA + lineC)) && (lineB > Math.abs(lineA - lineC)) || ((lineA < (lineB + lineC)) && (lineB < (lineA + lineC)) && (lineC < (lineA + lineB)));
+  } else if (lineC <= lineA && lineC <= lineB) {
+    return validaTriangulo = (lineC < (lineA + lineB)) && (lineC > Math.abs(lineA - lineB)) || ((lineA < (lineB + lineC)) && (lineB < (lineA + lineC)) && (lineC < (lineA + lineB)));
   }
-return validaTriangulo
 }
-
 // console.log(triangleCheck(10, 14, 8));
 // console.log(triangleCheck(8, 10, 14));
 // console.log(triangleCheck(8, 10, 140));
@@ -26,6 +23,7 @@ return validaTriangulo
 // console.log(triangleCheck(17, 4, 12));
 // console.log(triangleCheck(3, 5, 10));
 // console.log(triangleCheck(16, 9, 2));
+// console.log(triangleCheck(2, 2, 6));
 
 // Desafio 13
 function hydrate() {
